fbd24ba843b54cc8ac4ce336c83bb9bb484d1ba4
Andrey-Glukhov <agi2000@mail.ru>
2020-12-25 19:26:41 +0300

    change papers page

--- papers.php
+++ papers.php
@@ -2,12 +2,20 @@
 /**
 *Template Name: Research Papers Template
 */
-get_header(); ?>
+get_header(); 
 
+$background_names = array(
+  '/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/PaperGreen-min.png',
+  '/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/PaperGreen_1-min.png',
+  '/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/PaperGreen_2-min.png',
+  '/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/PaperGreen_3-min.png'
+);
+?>
+<>
 <div class="container-fluid">
-  <div class="row paper_wraper">
-    <div class="col-md-4">
-      <h1><span>RESEARCH</span><span>PAPERS</span></h1>
+  <div class="row paper_wrapper">
+    <div class="col-md-3">
+      <h1><span>RESEARCH</span> <span>PAPERS</span></h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
         faucibus mauris. Sed id libero eu lectus interdum vestibulum. Etiam sit
@@ -19,7 +27,8 @@ get_header(); ?>
         tristique lobortis at in leo.
       </p>
     </div>
-    <div class="col-md-8">
+    <div class="col-md-9 ">
+    <div class="row paper_row paper_board">
       <?php $args = array(
         'post_type' => 'papers_and_talks',
         'post_status' => 'publish',
@@ -28,11 +37,23 @@ get_header(); ?>
         'cat' => 6
       );
       $r_paper = new WP_Query( $args );
-      if($r_paper->have_posts() ) : while ( $r_paper->have_posts() ) : $r_paper->the_post();?>
-
-      <p><?php the_field('title'); ?></p>
-
-    <?php endwhile; ?>
+      $row_count =0;
+      if($r_paper->have_posts() ) : while ( $r_paper->have_posts() ) : $r_paper->the_post();
+        if ($row_count%3 == 0) :?>
+          
+        <?php endif;?>    
+      <div class="col-md-4 col-sm-6 col-xs-1 paper-box" data-row="<?php echo $row_count?>">
+      <a href="<?php the_permalink(); ?>" class="paper_link">
+      <div class="paper_background"> <img src="<?php echo $background_names[$row_count%4]; ?>"> </div>
+      <div class="paper_heading">  <p><?php the_field('title'); ?></p></div> 
+     </a>
+        </div>
+         <?php $row_count++;
+        if ($row_count%3 == 0) :?>
+          
+          <?php endif;  
+        endwhile; ?>
+          </div>
   <?php endif; ?>
 </div>
 </div>
