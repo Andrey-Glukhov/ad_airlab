16ce5dde7bb4b1fe00e79ddaf71be792b0dc63dc
Eliza <gluhova.liza@gmail.com>
2020-12-25 13:18:06 +0100

    team

--- connect.php
+++ connect.php
@@ -5,10 +5,8 @@
 get_header(); ?>
 
 <div class="container-fluid connect_container">
-    <div class="row justify-content-center">
-      <div class="col-lg-2 col-md-1 d-none d-sm-none d-md-block">
-      </div>
-      <div class="col-lg-4 col-md-6 col-sm-10">
+    <div class="row justify-content-center connect_row row-eq-height">
+      <div class="col-lg-4 col-md-5 col-sm-10 col-10">
         <?php
         $connect = new WP_Query( array( 'page_id' => 15 ) );
         if ( $connect->have_posts() ) : while ( $connect->have_posts() ) : $connect->the_post();
@@ -18,7 +16,8 @@ get_header(); ?>
         <?php endwhile; ?>
         <?php endif; ?>
       </div>
-      <div class="col-lg-6 col-md-5">
+      <div class="col-md-6 col-ms-11 col-10 map">
+        <img src="http://localhost:8888/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/connect_illustration-01.png" />
 
       </div>
 
--- meet_the_students.php
+++ meet_the_students.php
@@ -5,42 +5,52 @@
 get_header(); ?>
 
 <div class="container_fluid">
-<div class="row justify-content-center">
-  <div class="col-12 meet_st_pict"><img src="http://localhost:8888/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/Meet_Students-01.png"/>
+  <div class="row justify-content-center">
+    <div class="col-12 meet_st_pict"><img src="http://localhost:8888/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/Meet_Students-01.png"/>
+    </div>
   </div>
-</div>
-        <div class="row st_wraper">
-          <div class="st_line"></div>
+  <div class="row st_wraper">
+    <div class="st_line"></div>
 
-          <div class="col-6">Meet the Students</div>
+    <?php $args = array(
+      'post_type' => 'team',
+      'post_status' => 'publish',
+      'order' => 'ASC',
+      'posts_per_page' => -1,
+      'cat' => 3
 
-<?php $args = array(
-					'post_type' => 'team',
-					'post_status' => 'publish',
-					 'order' => 'ASC',
-					'posts_per_page' => -1,
-          'cat' => 3
-
-				);
-        $left_column = true;
-
-				$st_team = new WP_Query( $args );
-        if($st_team->have_posts() ) : while ( $st_team->have_posts() ) : $st_team->the_post();
-        if ($left_column){?>
-          <div class="col-6 st_team_profiles st_left">
-      <?php  }else{?>
-          <div class="col-6 st_team_profiles st_right">
-    <?php  }
-    $left_column = !$left_column;
+    );
+    $left_column = false;
+    $first_cycle= true;
+    $st_team = new WP_Query( $args );
+    if($st_team->have_posts() ) : while ( $st_team->have_posts() ) : $st_team->the_post();
+    if ($left_column){?>
+        <div class="col-6 st_team_profiles st_left">
+      <?php  }else{
+        if($first_cycle){?>
+          <div class="col-6 st_left">Meet</div>
+  <?php  $first_cycle =!$first_cycle;
+      } else{?>
+        <div class="col-6 st_left"></div>
+      <?php  }
+      ?>
+        <div class="col-6 st_team_profiles st_right">
+        <?php  }
         ?>
-            <img src="<?php the_field('portret'); ?>"/>
-            <p><?php the_title();?></p>
-              <p><?php the_content();?></p>
-          </div>
-
-      <?php endwhile; ?>
-<?php endif; ?>
-  </div>
+        <img src="<?php the_field('portret'); ?>"/>
+        <div class="student_data_wraper">
+        <p class="st_name"><?php the_title();?></p>
+        <div class="st_about"><?php the_content();?></div>
+        </div>
+      </div>
+      <?php  if ($left_column){?>
+        <div class="col-6 st_right"></div>
+          <?php  }
+          $left_column = !$left_column;
+          ?>
+    <?php endwhile; ?>
+  <?php endif; ?>
+</div>
 </div>
 
 
