746d889558ec8c5113def8117e842c7d6fa121eb
Andrey-Glukhov <agi2000@mail.ru>
2020-12-27 17:49:56 +0300

    change researchers layout

--- researchers.php
+++ researchers.php
@@ -6,10 +6,11 @@ get_header(); ?>
 
 <div class="container_fluid">
   <div class="row justify-content-center">
-    <div class="col-12 meet_st_pict"><img src="http://localhost:8888/AD_AIRLab/wordpress/wp-content/themes/ad_airlab/img/Meet_Students-01.png"/>
+    <div class="col-12 meet_researchers">
+      <h2>Meet the team</h2>
     </div>
   </div>
-  <div class="row st_wraper">
+  
     
 
     <?php $args = array(
@@ -23,35 +24,52 @@ get_header(); ?>
     $left_column = true;
     $first_cycle= true;
     $st_team = new WP_Query( $args );
-    if($st_team->have_posts() ) : while ( $st_team->have_posts() ) : $st_team->the_post();
-    if ($left_column){?>
-        <div class="col-6 st_team_profiles st_left">
-          <div class="student_data_wraper">
-            <p class="st_name"><?php the_title();?></p>
-            <div class="st_about"><?php the_content();?></div>
+    if($st_team->have_posts() ) : while ( $st_team->have_posts() ) : $st_team->the_post();?>
+    <div class="row rs_wraper">
+    <?php if ($left_column){?>
+      <div class="rs_liner_left"></div>
+        <div class="col-6 researchers_profiles rs_left">
+          <div class="researchers_data_wraper">
+            <p class="rs_name"><?php the_title();?></p>
+            <div class="rs_about"><?php the_content();?></div>
           </div>
         </div>
-        <div class="col-6 st_team_profiles st_right">
-          <div class="students_portret">
-            <img src="<?php the_field('portret'); ?>"/>
+        <div class="col-6 researchers_profiles rs_right">
+          <div class="researchers_portret">
+            <div class="portret_mark">
+              <img src="<?php the_field('portret'); ?>"/>
+              <div class="rs_mark_left">
+                <div class="rs_mark_outer_circle"></div>
+                <div class="rs_mark_inner_circle"></div>
+                <div class="rs_mark_dash"></div>
+              </div>
+            </div>  
           </div>
         </div>
       <?php  }else{?>
-        <div class="col-6 st_team_profiles st_left">
-          <div class="students_portret">
-            <img src="<?php the_field('portret'); ?>"/>
+        <div class="rs_liner_right"></div>
+        <div class="col-6 researchers_profiles rs_left">
+          <div class="researchers_portret">
+            <div class="portret_mark">
+              <img src="<?php the_field('portret'); ?>"/>
+              <div class="rs_mark_right">
+                <div class="rs_mark_outer_circle"></div>
+                <div class="rs_mark_inner_circle"></div>
+                <div class="rs_mark_dash"></div>
+              </div>
+            </div>  
+          </div> 
           </div>
-        </div>
-        <div class="col-6 st_team_profiles st_right">
-          <div class="student_data_wraper">
-            <p class="st_name"><?php the_title();?></p>
-            <div class="st_about"><?php the_content();?></div>
+        <div class="col-6 researchers_profiles rs_right">
+          <div class="researchers_data_wraper">
+            <p class="rs_name"><?php the_title();?></p>
+            <div class="rs_about"><?php the_content();?></div>
           </div>
         </div>
         <?php  }
         ?>
-        
-      <?php $left_column = !$left_column;
+    </div> 
+    <?php $left_column = !$left_column;
     endwhile; ?>
   <?php endif; ?>
 </div>
